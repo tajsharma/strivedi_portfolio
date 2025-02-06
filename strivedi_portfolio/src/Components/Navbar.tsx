cbca8dfa58f60f520ac376416607bdcb5a991481
Taj Sharma <sharma.tajinder01@gmail.com>
2025-02-05 19:54:42 -0800

    go back to css format instead of tailwind due to project due date

--- strivedi_portfolio/src/Components/Navbar.tsx
+++ strivedi_portfolio/src/Components/Navbar.tsx
@@ -1,10 +1,17 @@
 import React from "react";
 import './Navbar.css';
 
-
 function Navbar({}){
     return(
-        <>lebron james</>
+        <nav className="navbar">
+            <div className="navbar-container">
+                <a href="logo.com">
+                    <button className="logo"> Logo </button>
+                </a>
+
+                
+            </div>
+        </nav>
     )
 }
 
