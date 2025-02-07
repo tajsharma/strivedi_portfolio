808d8f7ae8a7867e927c8a235c785924f231727e
Taj Sharma <sharma.tajinder01@gmail.com>
2025-02-06 16:35:08 -0800

    place the home component to main app

--- strivedi_portfolio/src/App.tsx
+++ strivedi_portfolio/src/App.tsx
@@ -3,12 +3,14 @@ import reactLogo from './assets/react.svg'
 import viteLogo from '/vite.svg'
 import './App.css'
 import Navbar from './Components/Navbar'
+import Home from './Components/Home'
 
 function App() {
 
   return (
     <>
       <Navbar/>
+      <Home/>
     </>
   )
 }
