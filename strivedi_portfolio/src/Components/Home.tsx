8905ec5d5ab129207da954ff8f61b0a225050fac
Taj Sharma <sharma.tajinder01@gmail.com>
2025-02-06 16:35:19 -0800

    create base files for home page

---
+++ strivedi_portfolio/src/Components/Home.tsx
@@ -0,0 +1,18 @@
+import React from "react";
+
+function Home(){
+    return(
+        <div>
+            <div className="paralax-section">
+                <div className="main-image">
+                    <img className="" src="src/assets/Home/mainpagepic.png" alt="solo-pic"></img>
+                </div>
+                <div className="main-content-section">
+
+                </div>
+            </div>
+        </div>
+    )
+}
+
+export default Home
