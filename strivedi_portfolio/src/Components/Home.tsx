4f4485bd778be30bdd2e3e7a80f6d4a6e7852156
Taj Sharma <sharma.tajinder01@gmail.com>
2025-02-06 18:23:33 -0800

    place items onto html page for the home page

--- strivedi_portfolio/src/Components/Home.tsx
+++ strivedi_portfolio/src/Components/Home.tsx
@@ -1,13 +1,42 @@
 import React from "react";
+import './Home.css';
 
 function Home(){
     return(
         <div>
             <div className="paralax-section">
-                <div className="main-image">
-                    <img className="" src="src/assets/Home/mainpagepic.png" alt="solo-pic"></img>
+                <div className="main-image-div">
+                    <img className="solo-pic" src="src/assets/Home/mainpagepic.png" alt="solo-pic"></img>
                 </div>
                 <div className="main-content-section">
+                    <div className="hero-div">
+                        <div className="hero-header hero-main-text">
+                            hi, im <span className="header-nametag">shayna</span> ;
+                        </div>
+                        <div className="hero-para hero-main-text">
+                            a UI/UX designer who merges creativity 
+                            and technology to create seamless, intuitive 
+                            user experiences.
+                        </div>
+                        <div className="location-text">
+                            📍 currently based in DTX
+                        </div>
+                        <div className="brands-worked-with">
+                            <div className="italic-caption-text">
+                                (Who I’ve worked with)
+                            </div>
+
+                            <div className="trusted-by-header">
+                                TRUSTED BY
+                            </div>
+
+                            <div className="company-logo-container">
+                                
+                            </div>
+
+                        </div>
+
+                    </div>
 
                 </div>
             </div>
