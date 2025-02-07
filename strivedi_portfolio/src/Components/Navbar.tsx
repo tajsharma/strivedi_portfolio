a235f997f98b27de417f282dc05ed07a6fea3228
Taj Sharma <sharma.tajinder01@gmail.com>
2025-02-06 16:17:10 -0800

    place items on html page

--- strivedi_portfolio/src/Components/Navbar.tsx
+++ strivedi_portfolio/src/Components/Navbar.tsx
@@ -6,9 +6,28 @@ function Navbar({}){
         <nav className="navbar">
             <div className="navbar-container">
                 <a href="logo.com">
-                    <button className="logo"> Logo </button>
+                    <button className="logo poppins-semibold-nav-links"> Logo </button>
                 </a>
 
+                <div className="nav-links-container">
+                    <ul className="nav-list">
+                        <li>
+                            <a href="/home">
+                                <button className="nav-item poppins-semibold-nav-links">Home</button>
+                            </a>
+                        </li>
+                        <li>
+                            <a href="/about">
+                                <button className="nav-item poppins-semibold-nav-links">About</button>
+                            </a>
+                        </li>
+                        <li>
+                            <a href="/contact">
+                                <button className="nav-item poppins-semibold-nav-links">Get in Touch</button>
+                            </a>
+                        </li>
+                    </ul>
+                </div>
                 
             </div>
         </nav>
