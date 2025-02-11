5737a2b844c85bd920be211d45debf0d2d461aba
Taj Sharma <sharma.tajinder01@gmail.com>
2025-02-11 01:23:39 -0800

    change overflow

--- strivedi_portfolio/src/Components/Home.tsx
+++ strivedi_portfolio/src/Components/Home.tsx
@@ -5,11 +5,13 @@ import Navbar from './Navbar';
 function Home(){
     return(
         <div>
+            
             <div className="paralax-section">
                 <div className="main-image-div">
                     <img className="solo-pic" src="src/assets/Home/mainpagepic.png" alt="solo-pic"></img>
                 </div>
                 <div className="main-content-section">
+                    <Navbar/>
                     <div className="hero-div">
                         <div className="intro-text-div">
                             <div className="hero-header hero-main-text">
@@ -52,14 +54,66 @@ function Home(){
                                     <img className="logo-image" src="src/assets/Home/logos/bridgewestlogo.svg" alt="bridgewest group">
                                     </img>                         
                                 </div>
-                                
+                            </div>
+                        </div>
+                    </div>
+                </div>
+            </div>
+
+            <section className="projects-section">
+                <div className="projects-section-header">
+                    <div className="italic-caption-text">
+                        (Discover my work)
+                    </div>
+
+                    <div className="projects-header-text">
+                        PROJECTS
+                    </div>
+                </div>
+
+                <div className="project-section">
+                    <div className="project-container">
+                        <div className="project-info">
+                            <div className="project-name-text">
+                                Microsoft / NVIDIA VR CITY Simulation
+                            </div>
+                            <div className="project-caption-pill-container">
+                                <button className="inactive-pillbutton">
+                                    VR
+                                </button>
+                                <button className="inactive-pillbutton">
+                                    Tablet
+                                </button>
+                                <button className="inactive-pillbutton">
+                                    Desktop
+                                </button>
+                                <button className="inactive-pillbutton">
+                                    B2B
+                                </button>
+                                <button className="inactive-pillbutton">
+                                    Digital Twins
+                                </button>
+                                <button className="inactive-pillbutton">
+                                    3D Simulations
+                                </button>
+                            </div>
+
+                            <div className="project-description-para">
+                                Designed a user experience for the NVIDIA-powered Omniverse platform,
+                                showcasing how digital twin technology transforms industries through
+                                real-time 2D and 3D simulations.
                             </div>
 
                         </div>
 
+                        <div className="project-visual">
+                            <img src="src/assets/Home/devices/ipad.png" alt="ipad"></img>
+                        </div>
                     </div>
                 </div>
-            </div>
+
+
+            </section>
         </div>
     )
 }
