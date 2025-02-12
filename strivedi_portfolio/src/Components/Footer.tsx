985d1bf6c0fa780de4ab4eca5a833751ed9ecadc
Taj Sharma <sharma.tajinder01@gmail.com>
2025-02-12 14:43:17 -0800

    make mid and right div for the top of the footer

--- strivedi_portfolio/src/Components/Footer.tsx
+++ strivedi_portfolio/src/Components/Footer.tsx
@@ -20,13 +20,23 @@ function Footer(){
                         </button>
                         <p className="footer-nav-links-text">|</p>
                         <button className="footer-linkedin-logo">
-                            <img src="src/assets/Home/logos/linkedinicon.png"></img>
+                            <img src="src/assets/Home/logos/linkedinicon.png" alt="in"></img>
                         </button>
+                    </div>
+                    <div className="top-footer-wrapper-mid">
+                        <img src="src/assets/Home/logos/logoblack.png" alt="ST"></img>
+                    </div>
+                    <div className="top-footer-wrapper-right">
+                        <p className="footer-nav-links-text">
+                            © All Rights Reserved.
+                        </p>
                     </div>
                 </div>
 
-                <div className="footer-bottom">
-                    
+                <div className="footer-bottom-wrapper">
+                    <button className="footer-nav-links-text-smaller">
+                        Back to Top
+                    </button>
                 </div>
 
             </div>
